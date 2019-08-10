820d44f6bd3749bcbd2a8a1b7756ffc61f1d0acd
guiver <guiver95@gmail.com>
2019-08-10 19:23:31 -0400

    lista de usuarios

--- src/app/member-list/member-list.component.ts
+++ src/app/member-list/member-list.component.ts
@@ -1,4 +1,7 @@
+import { AlertifyService } from './../_services/alertify.service';
+import { UserService } from './../_services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../_models/User';
 
 @Component({
   selector: 'app-member-list',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor(private userService: UserService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.getUsers();
   }
 
+  getUsers() {
+    this.userService.getUsers().subscribe((users: User[]) => {
+      this.users = users;
+    }, err => {
+      this.alertify.error(err);
+    });
+  }
 }
