f1debecc34a944d74ab531df59d247abdb823045
guiver <guiver95@gmail.com>
2019-10-29 09:38:36 -0300

    lista de usuarios se pagina de a 10

--- src/app/_resolvers/member-list.resolver.ts
+++ src/app/_resolvers/member-list.resolver.ts
@@ -9,7 +9,7 @@ import { catchError } from 'rxjs/operators';
 @Injectable()
 export class MemberListResolver implements Resolve<User[]> {
   pageNumber = 1;
-  pageSize = 5;
+  pageSize = 10;
 
   constructor(private userService: UserService, private alertify: AlertifyService,
     private router: Router) { }
